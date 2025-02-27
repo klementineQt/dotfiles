9688f87cf4a2a01c5d20988142164b79c475162b
Klementine Des Pêches <klementineQt@proton.me>
2025-02-27 15:10:50 -0500

    i messed up earlier

--- functions/fishcfg.fish
+++
@@ -1,4 +0,0 @@
-function fishcfg --wraps='VISUAL /home/klementine/.config/fish/config.fish' --wraps='nano /home/klementine/.config/fish/config.fish' --description 'alias fishcfg nano /home/klementine/.config/fish/config.fish'
-  nano /home/klementine/.config/fish/config.fish $argv
-        
-end
