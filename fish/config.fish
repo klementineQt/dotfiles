bf88b4d60918fa7ad2a117192ec173f2c076bb1e
klementine des pêches <klementineQt@proton.me>
2025-09-25 06:13:43 -0400

    add tabby directory detection

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,29 @@
 # klementineQt (Klementine Des Pêches)
 # klementineQt@proton.me
 
+set -gx GPG_TTY $(tty)
+
 # init this nifty cd alternative (we're rusting away)
-zoxide init --cmd cd fish | source
+zoxide init --cmd z fish | source
 
-# can't live wifout me starship
-starship init fish | source
-
-# get a neat lil quote and make a cute lil cow say it
+# interactive shell specific
 if status is-interactive
-    fortune | cowsay
+    # configure editor env for vscode terminal usage, if remoted in
+    if set -q VSCODE_INJECTION
+        set -gx VISUAL "code --wait"
+        set -gx EDITOR "code --wait"
+    end
+
+    # can't live wifout me starship
+    starship init fish | source
+
+    # pyenv init
+    pyenv init - fish | source
+
+    # pyenv-virtualenv init
+    pyenv virtualenv-init - | source
+
+    function __tabby_working_directory_reporting --on-event fish_prompt
+        echo -en "\e]1337;CurrentDir=$PWD\x7"
+    end
 end
